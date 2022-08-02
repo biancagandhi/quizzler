6f0754f5ebe744663082e08a7268c83ed867fd42
biancagandhi <024biancagandhi@gmail.com>
2022-08-02 15:10:43 +0530

    firstcommit

---
+++ lib/main.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:quizzler/quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain = QuizBrain();
+
+void main() {
+  runApp(const Quizzler());
+}
+
+class Quizzler extends StatelessWidget {
+  const Quizzler({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    home: Scaffold(
+       backgroundColor: Colors.grey.shade900,
+      body:const SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.0),
+          child: QuizPage(),
+        ),
+      )
+    ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({Key? key}) : super(key: key);
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+
+  List<Icon> scoreKeeper = [];
+
+  void checkAnswer(bool userPickedAnswer){
+    bool correctAnswer = quizBrain.getCorrectAnswer();
+    setState((){
+
+      if(quizBrain.isFinished() == true){
+        Alert(
+          context: context,
+          title: 'Finished!',
+          desc: 'You\'ve reached the end of the quiz.',
+        ).show();
+
+        //TODO Step 4 Part C - reset the questionNumber,
+        quizBrain.reset();
+
+        //TODO Step 4 Part D - empty out the scoreKeeper.
+        scoreKeeper = [];
+      }else {
+        if (userPickedAnswer == correctAnswer) {
+          scoreKeeper.add(const Icon(Icons.check, color: Colors.green,));
+        } else {
+          scoreKeeper.add(const Icon(Icons.close, color: Colors.red,));
+        }
+
+        quizBrain.nextQuestion();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+       Expanded(
+      flex: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Center(
+          child: Text(
+            quizBrain.getQuestionText(),
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 25.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    ),
+
+        Expanded(
+            child: Padding(
+               padding: const EdgeInsets.all(15.0),
+               child: MaterialButton(
+               textColor: Colors.white,
+                color: Colors.green,
+                  child: const Text(
+                  'True',
+                   style: TextStyle(
+                  color: Colors.white,
+                   fontSize: 20.0,
+               ),
+             ),
+            onPressed: () {
+                checkAnswer(true);
+            },
+          ),
+        ),
+    ),
+
+        Expanded(
+            child: Padding(
+            padding: const EdgeInsets.all(15.0),
+                child: MaterialButton(
+                color: Colors.red,
+                    child: const Text(
+                    'False',
+                  style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+            onPressed: () {
+                    checkAnswer(false);
+                  },
+                ),
+            ),
+        ),
+        Row(
+          children: scoreKeeper,
+        )
+        ],
+    );
+  }
+}
+
